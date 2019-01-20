bd3688f45e4cbd1dda2a7210bd6415236d82a86d
Nicolas Jourdain <nicolas.jourdain@randstad.ca>
2019-01-19 23:19:30 -0500

    Get hashed processed ad urls

--- ScrapeAndNotifyTimerTrigger/index.js
+++ ScrapeAndNotifyTimerTrigger/index.js
@@ -1,4 +1,31 @@
-const kijiji = require("kijiji-scraper");
+const kijiji = require('kijiji-scraper');
+const https = require('https');
+
+function getHashedProcessedAdUrls(hashedProcessedAdsUrl, apiKey) {
+    return new Promise((resolve, reject) => {
+        https.get(
+            hashedProcessedAdsUrl,
+            {
+                headers: {
+                    'x-apikey': apiKey
+                }
+            },
+            (resp) => {
+                let data = '';
+
+                resp.on('data', (chunk) => {
+                    data += chunk;
+                });
+
+                resp.on('end', () => {
+                    resolve(JSON.parse(data));
+                });
+            }
+        ).on('error', (err) => {
+            reject(err.message);
+        });
+    });
+}
  
 module.exports = async function (context, myTimer) {
     let options = {
@@ -8,12 +35,19 @@ module.exports = async function (context, myTimer) {
     let params = {
         locationId: kijiji.locations.QUEBEC.GREATER_MONTREAL.CITY_OF_MONTREAL,
         categoryId: kijiji.categories.REAL_ESTATE.APARTMENTS_AND_CONDOS_FOR_RENT,
-        sortByName: "dateDesc",
-        keywords: "villeray",
+        sortByName: 'dateDesc',
+        keywords: 'villeray',
         minPrice: 1000
     };
 
     const ads = await kijiji.search(params, options);
+
+    const hashedProcessedAdUrls = await getHashedProcessedAdUrls(
+        process.env['HashedProcessedAdsUrl'],
+        process.env['RestdbApiKey']
+    );
+
+    context.log(hashedProcessedAdUrls);
 
     // Use the ads array
     for (let i = 0; i < ads.length; ++i) {
