433448b97be621cc5107e6a24979e71a460fde83
Nicolas Jourdain <nicolas.jourdain@randstad.ca>
2019-01-19 22:44:01 -0500

    Log ads for Villeray

--- ScrapeAndNotifyTimerTrigger/index.js
+++ ScrapeAndNotifyTimerTrigger/index.js
@@ -1,9 +1,22 @@
+const kijiji = require("kijiji-scraper");
+ 
 module.exports = async function (context, myTimer) {
-    var timeStamp = new Date().toISOString();
-    
-    if(myTimer.isPastDue)
-    {
-        context.log('JavaScript is running late!');
+    let options = {
+        minResults: 20
+    };
+     
+    let params = {
+        locationId: kijiji.locations.QUEBEC.GREATER_MONTREAL.CITY_OF_MONTREAL,
+        categoryId: kijiji.categories.REAL_ESTATE.APARTMENTS_AND_CONDOS_FOR_RENT,
+        sortByName: "dateDesc",
+        keywords: "villeray",
+        minPrice: 1000
+    };
+
+    const ads = await kijiji.search(params, options);
+
+    // Use the ads array
+    for (let i = 0; i < ads.length; ++i) {
+        context.log(ads[i].title);
     }
-    context.log('JavaScript timer trigger function ran!', timeStamp);   
 };
